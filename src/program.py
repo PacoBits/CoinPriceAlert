472caf20c434e9408f6ac027236abbd698abcf9b
Pacobits <fjducha@yahoo.es>
2021-08-06 09:26:46 +0200

    Update program.py

--- src/program.py
+++ src/program.py
@@ -14,15 +14,17 @@ BOT=os.getenv("BOT")
 CHANNEL=os.getenv("CHANNEL")
 
 a=0
+cg = CoinGeckoAPI()
 while(True):
-    cg = CoinGeckoAPI()
-    data=cg.get_price(ids=COINTICKET, vs_currencies=VS_CURRENCIE)
-    price=data[COINTICKET][VS_CURRENCIE]
-    if float(price)<float(LIMITPRICE):
-        response = requests.get('https://api.telegram.org/'+BOT+'/sendMessage?chat_id='+CHANNEL+'&text=Price '+COINTICKET +' lower than '+ str(LIMITPRICE)+ ' Value '+ str(price) )
-    a=a+1        
-    if a*(int(TIMESLEEP)/60/60/24)>1:
-        response = requests.get('https://api.telegram.org/'+BOT+'/sendMessage?chat_id='+CHANNEL+'&text=Price  '+COINTICKET +' reminder limit '+ str(LIMITPRICE)+ ' Value '+ str(price) )
-        a=0
-    time.sleep(int(TIMESLEEP))
-
+    try:    
+        data=cg.get_price(ids=COINTICKET, vs_currencies=VS_CURRENCIE)
+        price=data[COINTICKET][VS_CURRENCIE]
+        if float(price)<float(LIMITPRICE):
+            response = requests.get('https://api.telegram.org/'+BOT+'/sendMessage?chat_id='+CHANNEL+'&text=Price '+COINTICKET +' lower than '+ str(LIMITPRICE)+ ' Value '+ str(price) )
+        a=a+1        
+        if a*(int(TIMESLEEP)/60/60/24)>1:
+            response = requests.get('https://api.telegram.org/'+BOT+'/sendMessage?chat_id='+CHANNEL+'&text=Price  '+COINTICKET +' reminder limit '+ str(LIMITPRICE)+ ' Value '+ str(price) )
+            a=0
+        time.sleep(int(TIMESLEEP))
+    except:
+        time.sleep(int(200))
